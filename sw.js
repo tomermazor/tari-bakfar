c1f8f2ccdb727e02d5e63869b491f6ced4e01b9b
Tomer <tomer@tari-bakfar.local>
2026-05-27 21:06:38 +0300

    Bump SW cache to app-v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v6';
+const CACHE = 'app-v7';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
