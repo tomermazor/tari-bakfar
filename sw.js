59adc7916ee39203247b9cc428044c085d8c37e5
Tomer <tomer@tari-bakfar.local>
2026-05-29 19:46:15 +0300

    Simplify checks form: keep only supplier, amount, clear date, note

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v29';
+const CACHE = 'app-v30';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
