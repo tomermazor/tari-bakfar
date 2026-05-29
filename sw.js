374b609f9308a943029b194e423ddcd11f0d93e4
Tomer <tomer@tari-bakfar.local>
2026-05-29 19:36:59 +0300

    Fix sync: skip Row-not-found errors in pending queue so syncAll doesn't abort

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v27';
+const CACHE = 'app-v28';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
