d493a6e41e285c5ebb6c41ac9a3e72c868e9566e
Tomer <tomer@tari-bakfar.local>
2026-05-29 19:11:43 +0300

    UX fixes: remove add tab, scan manual entry btn, fix disabled buttons, advance date format, scroll reset, Enter keys

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v22';
+const CACHE = 'app-v26';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
