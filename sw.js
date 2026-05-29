39b79f8a488e5cdb2e27e1b9e7efc7cad9222f2f
Tomer <tomer@tari-bakfar.local>
2026-05-29 19:16:06 +0300

    Fix gallery input accept to use file extensions (reduces iOS Take Photo option)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v26';
+const CACHE = 'app-v27';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
