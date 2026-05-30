1e0ae3c8be598fb5e2ee07e5413798d70c5c16d5
Tomer <tomer@tari-bakfar.local>
2026-05-30 09:58:19 +0300

    Fix sticky nav: full flex layout, only main scrolls, no position:fixed needed

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v36';
+const CACHE = 'app-v37';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
