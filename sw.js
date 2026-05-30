1cd8c00914cfe9a19a058cb750e4b44abb1ed5c7
Tomer <tomer@tari-bakfar.local>
2026-05-30 10:23:30 +0300

    Weekly income navigation: browse past weeks, add income to any week, reports get last-week + year filters

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v39';
+const CACHE = 'app-v40';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
