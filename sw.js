d5d0fc67e63921129cb55b47c3a4e8855bf3e41d
Tomer <tomer@tari-bakfar.local>
2026-05-30 22:07:19 +0300

    Full sync: checks/income/advances to cloud, resilient syncAll, fix FAB, fix dashboard mobile, updated GAS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v44';
+const CACHE = 'app-v45';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
