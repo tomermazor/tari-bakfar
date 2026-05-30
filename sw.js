c188862bcab9063d7143a2095617372f7faabd27
Tomer <tomer@tari-bakfar.local>
2026-05-30 10:05:53 +0300

    Fix search jumps: surgical DOM update, input never destroyed, clear button, preventScroll focus

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v37';
+const CACHE = 'app-v38';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
