a82ba1bd57853f3685975183a0c4b469c5b1a8f7
Tomer <tomer@tari-bakfar.local>
2026-05-30 22:19:12 +0300

    Fix sync: skip 'unknown action' in PENDING, fix credit image iOS, debug logging

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v45';
+const CACHE = 'app-v46';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
