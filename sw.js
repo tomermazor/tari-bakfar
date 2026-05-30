3c2fcb30503b9e5aa3988797a83f1ea5efdb59d0
Tomer <tomer@tari-bakfar.local>
2026-05-30 07:11:26 +0300

    Batch multi-file scan: expenses and checks support multiple files with progress UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v31';
+const CACHE = 'app-v32';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
