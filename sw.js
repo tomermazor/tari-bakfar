c9905ff197928552ef78e9754424a389d6be24b0
Tomer <tomer@tari-bakfar.local>
2026-05-30 21:14:05 +0300

    Responsive desktop layout: sidebar nav, brand header, footer, grid layout at 768px+

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v41';
+const CACHE = 'app-v42';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
