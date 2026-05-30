68107a0c94176993b875d4f1f6881f3ad6ea1c2f
Tomer <tomer@tari-bakfar.local>
2026-05-30 21:53:09 +0300

    Desktop-optimized tabs: large sidebar buttons 52px, list table view, reports 2-col grid, fixed cards grid

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v43';
+const CACHE = 'app-v44';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
