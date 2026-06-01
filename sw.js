6e0085720085c6e83f63f7dab026b138274b20d5
Tomer <tomer@tari-bakfar.local>
2026-06-01 22:24:25 +0300

    fix: show supplier payment history on main screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v49';
+const CACHE = 'app-v50';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
