afbb1eb66648aaea6fddb3629c3b05668dd88909
Tomer <tomer@tari-bakfar.local>
2026-06-01 22:42:55 +0300

    fix: normalize payable dates and paid button taps

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v52';
+const CACHE = 'app-v53';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
