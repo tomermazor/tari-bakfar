2cafef33c6bb85397d1d80ee320f58a900a08c91
Tomer <tomer@tari-bakfar.local>
2026-06-01 22:35:16 +0300

    fix: simplify paid action and overdue colors

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v51';
+const CACHE = 'app-v52';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
