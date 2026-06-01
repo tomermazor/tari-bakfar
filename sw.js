4bc878d88572ac9fa4dcb07ac758f5d9c60af7ce
Tomer <tomer@tari-bakfar.local>
2026-06-01 22:28:22 +0300

    fix: keep pending supplier payments in open commitments

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v50';
+const CACHE = 'app-v51';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
