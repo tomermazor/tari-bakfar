cf7a20a823a5294def8bb5e5626fc7b07a923e52
Tomer <tomer@tari-bakfar.local>
2026-06-02 21:00:03 +0300

    fix: summarize only ordered supplier items

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v59';
+const CACHE = 'app-v60';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
