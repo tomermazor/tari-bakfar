1b23f59d105f5876c894bcb5dbb77dd79c979498
Tomer <tomer@tari-bakfar.local>
2026-06-02 21:45:45 +0300

    feat: require approved supplier cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v62';
+const CACHE = 'app-v63';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
