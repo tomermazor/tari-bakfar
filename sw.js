7772fc0cb1fabb9521b555412e7e3ccc988904d6
Tomer <tomer@tari-bakfar.local>
2026-06-02 20:52:40 +0300

    feat: add full order summary export

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v58';
+const CACHE = 'app-v59';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
