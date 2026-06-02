b9b33e0dd5737358ed04fed2bc09bdde72e2a005
Tomer <tomer@tari-bakfar.local>
2026-06-02 07:01:03 +0300

    fix: reopen mistaken paid supplier commitments

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v54';
+const CACHE = 'app-v55';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
