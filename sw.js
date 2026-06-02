71ccc26fc1c8460ef5d8963944490870ddf88ac1
Tomer <tomer@tari-bakfar.local>
2026-06-02 23:27:24 +0300

    feat: add desktop management shell

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v67';
+const CACHE = 'app-v68';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
