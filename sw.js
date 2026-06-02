81c69e9bd87ebcf341f08c3d1148ee5f05cb5a02
Tomer <tomer@tari-bakfar.local>
2026-06-02 21:25:28 +0300

    feat: link orders with delivery notes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v61';
+const CACHE = 'app-v62';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
