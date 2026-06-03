1cf6f88467c54eb5dfab0f264734934c85fb146b
Tomer <tomer@tari-bakfar.local>
2026-06-03 19:02:53 +0300

    feat: add orders-only user role

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v68';
+const CACHE = 'app-v69';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
