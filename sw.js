1ad3acc85904fd744f69fd87d0dbc0f45d30e000
Tomer <tomer@tari-bakfar.local>
2026-06-03 22:58:13 +0300

    fix: preserve desktop income during sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v75';
+const CACHE = 'app-v76';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
