9864fddfbd19a47ddc121047d3929ad8c6776536
Tomer <tomer@tari-bakfar.local>
2026-06-03 22:13:13 +0300

    feat: add professional desktop workspace views

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v71';
+const CACHE = 'app-v72';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
