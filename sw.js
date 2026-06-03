2993e31249f3a1bec26834eee2d65f33a31818db
Tomer <tomer@tari-bakfar.local>
2026-06-03 22:34:22 +0300

    fix: sync desktop data and improve sidebar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v72';
+const CACHE = 'app-v73';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
