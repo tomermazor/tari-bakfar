8eecc19f30f2897174db20a5c8ae8fe65d20886a
Tomer <tomer@tari-bakfar.local>
2026-06-03 23:37:33 +0300

    feat: rebuild product command center

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v76';
+const CACHE = 'app-v77';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
