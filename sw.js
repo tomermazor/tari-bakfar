e2ac022024f2a97fc346e2302783cedaa1e3b818
Tomer <tomer@tari-bakfar.local>
2026-06-03 21:50:19 +0300

    fix: restore expense scan button access

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v70';
+const CACHE = 'app-v71';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
