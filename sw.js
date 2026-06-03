5f4f8f325440c24f2a6439fd94a58ecc3accd2fd
Tomer <tomer@tari-bakfar.local>
2026-06-03 19:18:55 +0300

    fix: enforce orders-only access

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v69';
+const CACHE = 'app-v70';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
