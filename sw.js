a998ae27c41ba96041dab0e2451ef3bc8ec08353
Tomer <tomer@tari-bakfar.local>
2026-06-04 11:54:33 +0300

    fix: recover latest financial rows and date labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v85';
+const CACHE = 'app-v86';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
