1207b647f312666d03a386850f951104bd42549a
Tomer <tomer@tari-bakfar.local>
2026-06-04 11:19:04 +0300

    fix: smooth product search input

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v82';
+const CACHE = 'app-v83';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
