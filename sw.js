c7541b171368d6b1d8eaebed09f55201d63a74c6
Tomer <tomer@tari-bakfar.local>
2026-06-04 12:30:41 +0300

    fix: display sheet timestamps in Israel timezone

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v86';
+const CACHE = 'app-v87';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
