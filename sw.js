bf158918ca6266f3ca37fd5b61737ba7355db3bc
Tomer <tomer@tari-bakfar.local>
2026-06-04 11:36:20 +0300

    fix: prevent empty cloud state wiping data

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v83';
+const CACHE = 'app-v84';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
