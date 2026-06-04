840117e2011b3a813c695fe82404168f49316762
Tomer <tomer@tari-bakfar.local>
2026-06-04 11:42:32 +0300

    fix: merge legacy sheet data into app state sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v84';
+const CACHE = 'app-v85';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
