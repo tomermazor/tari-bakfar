f3e103a187c1543fcaf8fdbdfcaac98a69c09221
Tomer <tomer@tari-bakfar.local>
2026-06-04 23:24:12 +0300

    fix: preserve and resync income data

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v88';
+const CACHE = 'app-v89';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
