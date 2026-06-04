467177ada1a3a80af4cedf03f99399f976c3b4ae
Tomer <tomer@tari-bakfar.local>
2026-06-04 10:26:21 +0300

    feat: add bulk product editing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v79';
+const CACHE = 'app-v80';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
