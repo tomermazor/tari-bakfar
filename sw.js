5c5912dfee14788fb8e9cc21d812272a218d4182
Tomer <tomer@tari-bakfar.local>
2026-06-06 07:23:23 +0300

    feat: add full app backup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v90';
+const CACHE = 'app-v91';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
