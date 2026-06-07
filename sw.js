12d39139e076f68b5be22e3dfcc5d22f2e3cd56e
Tomer <tomer@tari-bakfar.local>
2026-06-07 21:38:01 +0300

    feat: close linked active orders

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v95';
+const CACHE = 'app-v96';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
