53e672de5c7ee36a646b806b944d92ea7a9fef1d
Tomer <tomer@tari-bakfar.local>
2026-06-07 14:32:37 +0300

    fix: stabilize manual order product input

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v94';
+const CACHE = 'app-v95';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
