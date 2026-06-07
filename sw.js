b8f5a5aa8cca8148ad24abc217e56f3447148afa
Tomer <tomer@tari-bakfar.local>
2026-06-07 13:31:43 +0300

    feat: add approved supplier from scan form

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v93';
+const CACHE = 'app-v94';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
