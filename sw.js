39241ae65639b5ff351f0befcc797e291bfa9e20
Tomer <tomer@tari-bakfar.local>
2026-06-08 16:31:32 +0300

    fix: keep expense scans local during quota errors

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v99';
+const CACHE = 'app-v100';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
