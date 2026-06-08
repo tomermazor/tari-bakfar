2a91160aa8bdb7c7168b1cc601df48e5469d3452
Tomer <tomer@tari-bakfar.local>
2026-06-08 13:46:16 +0300

    feat: add multi expense scanning flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v97';
+const CACHE = 'app-v98';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
