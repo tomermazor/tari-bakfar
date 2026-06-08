4d9b74038a1b42e3e1fa5c195647663c54368dee
Tomer <tomer@tari-bakfar.local>
2026-06-08 14:29:53 +0300

    fix: prevent duplicate scanned expenses

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v98';
+const CACHE = 'app-v99';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
