e1ef48848bffbb37c1a515ecc1549acb762506b7
Tomer <tomer@tari-bakfar.local>
2026-06-09 15:11:52 +0300

    feat: add safe supplier invoice deletion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v111';
+const CACHE = 'app-v112';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
