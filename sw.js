a238ebe008efce2d757a19c5e55ab334a7754527
Tomer <tomer@tari-bakfar.local>
2026-06-09 11:27:25 +0300

    feat: add safe supplier invoice tab

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v106';
+const CACHE = 'app-v107';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
