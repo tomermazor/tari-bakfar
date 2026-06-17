b2a3f4b1bac4fa8e470d7e7855c60ab8883ef17f
Tomer <tomer@tari-bakfar.local>
2026-06-17 10:48:35 +0300

    show weekly expenses total in expense list

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v114';
+const CACHE = 'app-v115';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './manifest.json'];
 
 self.addEventListener('install', e => {
