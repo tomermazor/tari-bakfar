5b769bd76596cf5fb3ee05e05241a624ba9a6f48
Tomer <tomer@tari-bakfar.local>
2026-07-05 11:17:18 +0300

    Simplify payables add invoice action

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v126';
+const CACHE = 'app-v127';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './products.html', './payables.html', './manifest.json'];
 
 self.addEventListener('install', e => {
