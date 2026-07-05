abcd477daca7ecde8a494dc0634cf2d9e16e1fe1
Tomer <tomer@tari-bakfar.local>
2026-07-05 11:36:13 +0300

    Smooth paid payable confirmation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v127';
+const CACHE = 'app-v128';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './products.html', './payables.html', './manifest.json'];
 
 self.addEventListener('install', e => {
