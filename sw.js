dc29a2bef5327491e55dca8a4dd53d6386423855
Tomer <tomer@tari-bakfar.local>
2026-07-05 10:07:38 +0300

    Fix paid supplier invoice action

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v121';
+const CACHE = 'app-v122';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './products.html', './payables.html', './manifest.json'];
 
 self.addEventListener('install', e => {
