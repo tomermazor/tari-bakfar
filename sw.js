0c8ce0e92c632a3ac7992edd2a808f058697f661
Tomer <tomer@tari-bakfar.local>
2026-07-05 10:19:10 +0300

    Make paid invoice confirmation modal reliable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v123';
+const CACHE = 'app-v124';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './products.html', './payables.html', './manifest.json'];
 
 self.addEventListener('install', e => {
