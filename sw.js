6eb500f8421aefa9c285508f41e25c6043b4bbab
Tomer <tomer@tari-bakfar.local>
2026-07-05 12:02:18 +0300

    Make paid payables disappear reliably

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v128';
+const CACHE = 'app-v129';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './products.html', './payables.html', './manifest.json'];
 
 self.addEventListener('install', e => {
