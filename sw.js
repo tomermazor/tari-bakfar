3e492d1a2402011958262ce1bb3e2049576b7729
Tomer <tomer@tari-bakfar.local>
2026-07-06 13:13:18 +0300

    Add global action feedback guards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v131';
+const CACHE = 'app-v132';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './products.html', './payables.html', './manifest.json'];
 
 self.addEventListener('install', e => {
