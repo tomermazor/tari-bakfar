0368fbe4466094d8957acafe8a8f5c6687e4189b
Tomer <tomer@tari-bakfar.local>
2026-07-06 13:23:55 +0300

    Add payables search button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v132';
+const CACHE = 'app-v133';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './products.html', './payables.html', './manifest.json'];
 
 self.addEventListener('install', e => {
