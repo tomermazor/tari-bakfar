d0b48a39c9711d70c0ab32e313f221049363761d
Tomer <tomer@tari-bakfar.local>
2026-07-06 12:19:44 +0300

    Create supplier payables from expense invoices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v130';
+const CACHE = 'app-v131';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './products.html', './payables.html', './manifest.json'];
 
 self.addEventListener('install', e => {
