d47d1ad56b824584339bd38786d7915d452126b0
Tomer <tomer@tari-bakfar.local>
2026-07-19 13:54:16 +0300

    Restore scrollable mobile navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v143';
+const CACHE = 'app-v144';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './products.html', './payables.html', './app-modern.css', './manifest.json'];
 
 self.addEventListener('install', e => {
