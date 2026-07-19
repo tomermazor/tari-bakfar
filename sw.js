a0f499df8ca33aafc3964cbb81311440a5cdf00c
Tomer <tomer@tari-bakfar.local>
2026-07-19 14:21:38 +0300

    Recover scanned expense saves from Safari quota

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v145';
+const CACHE = 'app-v146';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './products.html', './payables.html', './app-modern.css', './manifest.json'];
 
 self.addEventListener('install', e => {
