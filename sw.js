613f1222418b270ec64d23e520874df66c2657cd
Tomer <tomer@tari-bakfar.local>
2026-07-19 12:44:51 +0300

    Protect supplier checks from sync loss

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-v142';
+const CACHE = 'app-v143';
 const SHELL = ['./index.html', './tari-bakfar.html', './orders.html', './products.html', './payables.html', './app-modern.css', './manifest.json'];
 
 self.addEventListener('install', e => {
